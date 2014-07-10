a332f4065d8e6a6a235c9d92cb51db900d655e6e
nyandrew <szandrewmai@gmail.com>
2014-07-10 16:22:06 -0400

    course project 1

---
+++ plot2.R
@@ -0,0 +1,28 @@
+library(datasets)
+with(airquality,plot(Wind,Ozone,main="Ozone in New York City"))
+title(main="Ozone and Wind in New York City")
+with
+
+data = read.table("household_power_consumption.txt", header = TRUE, sep = ";",)
+head(data)
+str(data)
+data = transform(data,Date = as.Date(as.character(Date),"%d/%m/%Y"))
+print(object.size(data), unit="Mb")
+
+rawfile = file("household_power_consumption.txt", "r")
+cat(grep("(^Date)|(^[1|2]/2/2007)", readLines(rawfile),value = TRUE), sep = "\n", file = "filtered.txt")
+close(rawfile)
+data = read.table("filtered.txt", header = TRUE, sep = ";",na.strings="?")
+head(data)
+str(data)
+dim(data)
+data = transform(data,Date = as.Date(as.character(Date),"%d/%m/%Y"))
+data = transform(data,Time = as.character(Time))
+mean(is.na(data))
+time <- strptime(paste(data$Date,data$Time), "%d/%m/%Y %H:%M:%S")
+data = data[,-c(1,2)]   ##Or data <- subset(data,select=-c(Date,Time))
+data$time = time
+data <- data[,c(8,1,2,3,4,5,6,7)]
+plot(data$time, data$Global_active_power,type="l", ylab = "Global Active Power (kilowatts)",xlab="")
+dev.copy(png,"plot2.png")
+dev.off()
